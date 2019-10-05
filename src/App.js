afce0cb2ffda3deccb51414ffbf6b5d4b6866ba9
Shatalov Viktor <shatalov.viktor@gmail.com>
2019-10-05 14:19:53 +0300

    init repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className="container">
+        <header>React app</header>
+        <h1>Hello, world!</h1>
+      </div>
+    )
+  }
+}
+
+export default App
